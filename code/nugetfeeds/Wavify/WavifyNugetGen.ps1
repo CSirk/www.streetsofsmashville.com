68f872b3919add6d71d881ed544e8d398f7b35a1
Cody Sirk <csirk005@gmail.com>
2018-10-06 22:50:01 -0500

    testing nuget feed concept

---
+++ code/nugetfeeds/Wavify/WavifyNugetGen.ps1
@@ -0,0 +1,8 @@
+﻿$projectPath = "C:\Users\codas\Dropbox\Programming\Git\CSharp\Wavify";
+$nugetExecutablePath = "C:\Users\codas\Dropbox\Programming\Git\CSharp\Wavify\" + "nuget.exe";
+$nuspecFile = "Wavify.nuspec";
+$version = "1.2.0";
+
+
+cd $projectPath;
+&$nugetExecutablePath pack $nuspecFile -Version $version;
